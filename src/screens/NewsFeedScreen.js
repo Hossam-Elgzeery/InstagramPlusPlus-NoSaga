fabda1e7611ab32c7ea250144af601695b93c917
Hossam Abdel-Aziz <Hossam.elgzeery@live.com>
2021-01-30 00:30:07 +0200

    newsfeed Boilerplate

--- src/screens/NewsFeedScreen.js
+++ src/screens/NewsFeedScreen.js
@@ -1,22 +1,84 @@
-import React from 'react'
-import {StyleSheet,View,Text,Image} from 'react-native'
-
-
-
+import React,{useEffect,useState} from 'react'
+import {StyleSheet,View,Text,Image,ActivityIndicator,Alert,TouchableOpacity} from 'react-native'
 import {widthRate,heightRate} from '../helperfunc/screenSizes';
+import HeadBar from '../components/HeadBar'
 
+import {useSelector} from 'react-redux';
+import fakeapi from '../apis/fakeapi'
+import UserCard from '../components/UserCard';
 
 
 const NewsFeedScreen=()=>{
 
+    
+    const [dataST,setData]=useState(null);
+    const [loadingST,setLoading]=useState(true);
+    const[reloadST,setReload]=useState(false);
+
+    const getData=async()=>{
+
+           setLoading(true);
+            try{
+                const response=await fakeapi.get(`/users?id=${id}`);
+    
+               if (response.data.length>0)
+               {
+               
+                setReload(false);
+                setUser(response.data[0]);
+                
+               
+               }
+               else 
+               {
+                setReload(true);
+                Alert.alert('No Data','No Data Found');
+               }
+            }
+            catch(e)
+            {
+               
+                Alert.alert('Error',e.message);
+                setReload(true);
+            }
+
+            finally
+            {
+                setLoading(false);
+            }
+        
+        }
+  
+
+    useEffect(()=>{
+
+        getData();
+
+    },[]);
   
  
     return(
     <View style={styles.mainContainer}>
+        <HeadBar title='Profile' />
+        {
+            (loadingST)?
 
-        <Image source={require('../assets/logo.png')}  style={styles.logo}/>
-
-        <Text>News Feed Screen</Text>
+                <ActivityIndicator style={styles.loadingStyle} size="large" color="#d64045" />
+                :
+                (reloadST)?
+                    <TouchableOpacity style={styles.refreshbtn} onPress={()=>{
+                
+                        getData();
+                    }} >
+                        
+                            <Text style={styles.btnText}>Retry</Text>
+                        
+                    </TouchableOpacity>
+                :
+                    <Text>Data Found</Text>
+        
+        }
+        
 
     </View>
     )
@@ -30,12 +92,29 @@ mainContainer:{
     
 },
 
-logo:{
-    height:heightRate(40),
-    width:widthRate(50),
-    resizeMode:'cover'
+
+loadingStyle:{
+  
+    marginTop:heightRate(5),
+},
+
+refreshbtn:{
+    backgroundColor:'#d64045',
+    height:heightRate(5),
+    width:widthRate(30),
+    alignContent:'center',
+    justifyContent:'center',
+    borderRadius:10,
+    marginTop:heightRate(5),
     
-}
+},
+btnText:{
+    color:'#F7F7FF',
+    fontSize:20,   
+    textAlign:'center',
+   
+    
+},
 
 
 });
