a1f6d18473a4cf7a1a421247d2fd7995adafba1d
Hossam Abdel-Aziz <Hossam.elgzeery@live.com>
2021-01-29 19:30:43 +0200

    modify testScreen

--- src/screens/TestScreen.js
+++ src/screens/TestScreen.js
@@ -1,12 +1,10 @@
-import React,{useEffect,useState} from 'react'
-import {Alert,StyleSheet,View,TextInput,TouchableOpacity,Text,ActivityIndicator,Image} from 'react-native'
+import React from 'react'
+import {StyleSheet,View,Text,Image} from 'react-native'
 
 
-import { useNavigation,useRoute } from '@react-navigation/native';
+
 import {widthRate,heightRate} from '../components/screenSizes';
-import {useDispatch} from 'react-redux'
 
-import fakeapi from '../apis/fakeapi'
 
 
 const TestScreen=()=>{
